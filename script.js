cf915d344add96bbbfbd637c0efe5ca4325d905f
DongJuKR <102017209+DongJuKR@users.noreply.github.com>
2024-05-27 10:03:22 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ function submitForm() {
   let resultFlag = false;
 
   var inputValue1 = document.getElementById("00NIR000009MV5P").value;
-  var inputValue2 = document.getElementById("00NIR000009MV5U").value;
   var inputValue3 = document.getElementById("00NIR000009MV5e").value;
   var inputValue4 = document.getElementById("00NIR000009MV5j").value;
   var inputValue5 = document.getElementById("subject").value;
   
-  if (!inputValue1 || !inputValue2 || !inputValue3 || !inputValue4 || !inputValue5) {
+  if (!inputValue1 || !inputValue3 || !inputValue4 || !inputValue5) {
     alert("필수 입력 값이 비어 있습니다. 입력 값을 확인해주세요.");
     return resultFlag;
   } else {
